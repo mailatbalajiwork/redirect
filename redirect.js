424de969821e304086bcd70c14780f77fd123453
mailatbalajiwork <mail@balaji.work>
2023-03-02 00:15:40 +0530

    Changing rediect.js with case switch

--- redirect.js
+++ redirect.js
@@ -1,19 +1,14 @@
-	const url = window.location.href;
-const suffix = url.substring(url.lastIndexOf('/') + 1);
-
-switch (suffix) {
+switch (condition) {
   case 'bimtl':
-    window.location.href = 'http://bim.hoch.in/bimtl';
+    window.location.href = 'https://bim.hoch.in/bimtl';
     break;
   case 'bimws':
-    window.location.href = 'http://bim.hoch.in/bimws';
+    window.location.href = 'https://bim.hoch.in/bimws';
     break;
   case 'seppt':
-    window.location.href = 'http://bim.hoch.in/seppt';
+    window.location.href = 'https://bim.hoch.in/seppt';
     break;
-  case 'ammom':
-    window.location.href = 'http://bim.hoch.in/ammom';
+  default:
+    window.location.href = 'https://git.hoch.in/';
     break;
-	default:
-    console.log('No redirect necessary');
 }
